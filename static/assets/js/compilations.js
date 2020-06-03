4ab90b795fd2ee2aaee1bddb37a2c7cab5850e7c
Anton Burchak <anton.burchak01@gmail.com>
2020-06-03 18:36:19 +0300

    finished compilations

---
+++ static/assets/js/compilations.js
@@ -0,0 +1,107 @@
+function renderAlbums(data) {
+const parent = document.querySelector('.comp .main__albums-content');
+parent.innerHTML = null;
+
+    data.forEach(album => {
+        const pattern = `
+                    <div class="main__item">
+                        <div><p>${album.author}</p></div>
+                         <img src="${album.picture}" alt="">
+                    </div>
+        `;
+        parent.innerHTML += pattern;
+    });
+
+}
+function renderDefaultAlbums() {
+    postData('http://0.0.0.0:5000/api/v1/compilations/albums?page=1', {}, 'GET')
+        .then((data) => {
+            console.log(data); // JSON data parsed by `response.json()` call
+            renderAlbums(data.albums);
+
+        }).catch((data) => {
+            console.error(data);
+            console.trace();
+        });
+}
+renderDefaultAlbums();
+
+function renderComp(data) {
+const parent = document.querySelector('.comp .main__random .main__albums-content');
+    parent.innerHTML = null;
+
+    data.forEach(comp => {
+        const pattern = `
+                    <div class="main__item">
+                        <a href="${comp.link}" style="color:white">
+                            <div style="display:flex; flex-direction: column;">
+                            <p style="font-size: 20px">${comp.channel}</p>
+                            <p>${comp.video_name}</p>
+                            </div>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQil5hfR-TPbe5IvxA_qvx9VUG7p3kYfpMU6C6odqnVKl7fWehK&usqp=CAU" alt="">
+                        </a>
+                    </div>
+        `;
+        parent.innerHTML += pattern;
+    });
+
+}
+function renderDefaultComp() {
+    postData('http://0.0.0.0:5000/api/v1/compilations/yt?page=1', {}, 'GET')
+        .then((data) => {
+            console.log(data); // JSON data parsed by `response.json()` call
+            renderComp(data.compilations);
+
+        }).catch((data) => {
+            console.error(data);
+            console.trace();
+        });
+}
+renderDefaultComp();
+
+
+const input1 = document.querySelector('.main__albums .search input');
+const btn1 = document.querySelector('.main__albums .search .btn');
+
+input1.addEventListener('input', function() {
+    if (input1.value.length !== 0) {
+        btn1.style.display = 'inline-block'
+    } else {
+        btn1.style.display = 'none';
+        renderDefault();
+    }
+});
+
+btn1.addEventListener('click', () => {
+    postData(`http://0.0.0.0:5000/api/v1/compilations/albums/search?page=1&text=${input1.value}`, {}, 'GET')
+        .then((data) => {
+            console.log(data);
+            renderAlbums(data.albums)
+        }).catch((data) => {
+            console.error(data);
+            console.trace();
+        });
+});
+
+const input = document.querySelector('.main__random .search input');
+const btn = document.querySelector('.main__random .btn');
+
+input.addEventListener('input', function() {
+    if (input.value.length !== 0) {
+        btn.style.display = 'inline-block'
+    } else {
+        btn.style.display = 'none';
+        renderDefault();
+    }
+});
+
+btn.addEventListener('click', () => {
+    postData(`http://0.0.0.0:5000/api/v1/news/search?page=1&text=${input.value}`, {}, 'GET')
+        .then((data) => {
+            console.log(data);
+            renderComp(data.compilations)
+        }).catch((data) => {
+            console.error(data);
+            console.trace();
+        });
+});
