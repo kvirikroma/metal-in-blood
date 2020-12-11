4e890d605fa14202c94f99b4b870d29010d8a085
pacifi5t <thequasar7@gmail.com>
2020-12-11 23:21:17 +0200

    profile rendered correctly

--- static/assets/js/profile.js
+++ static/assets/js/profile.js
@@ -1,21 +1,10 @@
 const user_id = sessionStorage.getItem("user_id");
 const name = sessionStorage.getItem("current_user");
 
-draw_profile();
+get_data();
 
-function draw_profile()
+function draw_profile(pic_source, language)
 {
-    var pic_src = "";
-    postData(`/api/v1/user?id=${user_id}`, {}, 'GET')
-    .then((data) => {
-        console.log(data);
-        pic_src += data.account_picture;
-
-    }).catch((data) => {
-        console.error(data);
-        console.trace();
-    });
-
     var first = "";
     var second = "";
     var i = 0;
@@ -30,9 +19,22 @@ function draw_profile()
     const parrent = document.querySelector('.profile .main__content');
     parrent.innerHTML = '';
     const pattern = `
-    <p><img src="${pic_src}" width="300" height="300"></p>
-    <p><span style="font-family: 'Doom-L'; font-size: 32pt">${first}</span><span style="font-family: 'Doom-R'; font-size: 32pt">${second}</span></p>
-
+    <div><img src="${pic_source}" width="300" height="300"></div>
+    <div><span style="font-family: 'Doom-L'; font-size: 32pt">${first}</span><span style="font-family: 'Doom-R'; font-size: 32pt">${second}</span></div>
+    <div>Preferable language: ${language}</div>
     `;
     parrent.innerHTML += pattern;
+}
+
+function get_data()
+{ 
+    postData(`/api/v1/user?id=${user_id}`, {}, 'GET')
+    .then((data) => {
+        console.log(data);
+        draw_profile(data.account_picture, data.language);
+
+    }).catch((data) => {
+        console.error(data);
+        console.trace();
+    });    
 }
