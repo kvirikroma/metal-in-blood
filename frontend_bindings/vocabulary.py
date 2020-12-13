e9793ef523403e38f6826c4e6804839477e26423
crazylinuxer <kvirikroma@gmail.com>
2020-12-13 18:44:16 +0200

    templates rendering

---
+++ frontend_bindings/vocabulary.py
@@ -0,0 +1,77 @@
+from flask import Markup
+
+en = {
+    "add_comment": "Add comment",
+    "add_thread": "Add thread",
+    "search": "Search",
+    "albums": Markup('<h2>Albums</h2>'),
+    "rand_comp": "Random Compilations",
+    "add_news": "Add news",
+    "heading": "Heading",
+    "pic_src": "Picture source",
+    "must_contain": "Must contain:",
+    "lowercase": "- lowercase",
+    "symbols": "- >= 6 symbols",
+    "sign_up": "Sign Up",
+    "already": "Already registered?",
+    "login": "Login",
+    "sign_in": "Sign In",
+    "account": "Don't have an account?",
+    "register": "Register",
+    "type_comment": "Type yout comment here",
+    "search_alb": "Search albums...",
+    "search_comp": "Search compilations...",
+    "search_trd": "Search threads...",
+    "type_heading": "Type heading here",
+    "thread_text": "Enter thread text here",
+    "text": "Text",
+    "search_news": "Search news...",
+    "news_picture": "Enter picture source here",
+    "type_text": "Type text here",
+    "password": "Password",
+    "email": "Email",
+    "password_repeat": "Repeat password",
+    "search_tips": "Search tips...",
+    "main_description": 'Welcome to the "Metal in blood" - place for the metalheads from all over the world! Are you a '
+                        'guitarist, vocalist, drumer or just a fan - it doesn`t matter because in any case you can find '
+                        'here good tips, music compilaions and like-minded people. Go to the page you like and let`s see '
+                        'if there some metal in your blood 😈'
+}
+
+ua = {
+    "add_comment": "Додати коментар",
+    "add_thread": "Додати тред",
+    "search": "Пошук",
+    "albums": Markup('<h2>Альбоми</h2>'),
+    "rand_comp": "Випадкові добірки",
+    "add_news": "Додати новину",
+    "heading": "Тема",
+    "pic_src": "Джерело зображення",
+    "must_contain": "Повинно містити:",
+    "lowercase": "- нижній регістр",
+    "symbols": "- >= 6 символів",
+    "sign_up": "Зареєструватися",
+    "already": "Вже маєте профіль?",
+    "login": "Логін",
+    "sign_in": "Увійти",
+    "account": "Не зареєстровані?",
+    "type_comment": "Введіть свій коментар",
+    "search_alb": "Пошук альбомів...",
+    "search_comp": "Пошук добірок...",
+    "search_trd": "Пошук тредів...",
+    "type_heading": "Введіть тему",
+    "thread_text": "Введіть текст",
+    "text": "Текст",
+    "search_news": "Пошук новин...",
+    "news_picture": "Введіть джерело зображення",
+    "type_text": "Введіть текст",
+    "password": "Пароль",
+    "email": "Ел. адреса",
+    "password_repeat": "Повторення паролю",
+    "search_tips": "Пошук порад...",
+    "main_description": 'Ласкаво просимо до "Metal in blood" - місця для металістів з усього світу! '
+                        'Ти гітарист, вокаліст, барабанщик чи просто фанат - неважливо, адже в будь-якому '
+                        'випадку тут ти можеш знайти хороші поради, музичні компіляції та однодумців. '
+                        'Хутчіш переходь на ту сторінку, яка тобі подобається, і давай подивимося, '
+                        'чи є в твоїй крові метал 😈'
+}
